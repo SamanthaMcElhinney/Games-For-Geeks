0e7d1e1955bf0f3055ab08bef5b0d39ad82bb070
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-05 21:04:15 -0700

    state for game type and basic jsx elements of the home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -8,16 +8,28 @@ const HomePage = () => {
         setSelectedGameType(gameType)
     }
 return (
-    <section className='home-page-container'>
-        <div className='slogan-container'>
-            <h2 className='slogan'>Unlock the Fun: Select Your Board Game Adventure!</h2>
-        </div>
-            <div>
-                <button onClick={() => handleGameTypeSelection('solo')}>Solo</button>
-            </div>
-    </section>
-
-)
+  <section className="home-page-container">
+    <div className="slogan-container">
+      <h2 className="slogan">
+        Unlock the Fun: Select Your Board Game Adventure!
+      </h2>
+    </div>
+    <div className="game-type-container">
+      <section className="solo" onClick={() => handleGameTypeSelection("solo")}>
+        <h3 className="title-solo">Solo</h3>
+      </section>
+      <section className="duo" onClick={() => handleGameTypeSelection("duo")}>
+        <h3 className="title-duo">Duo</h3>
+      </section>
+      <section
+        className="party"
+        onClick={() => handleGameTypeSelection("party")}
+      >
+        <h3 className="title-party">Party</h3>
+      </section>
+    </div>
+  </section>
+);
 }
 
 export default HomePage
