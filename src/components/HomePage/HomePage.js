a9bc3a751a1333871028a94ab15735cd68e37ee2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 09:36:47 -0700

    feat: link to route to duo page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,13 +3,15 @@ import './HomePage.css'
 import Dice1 from "../../assets/Dice1.png";
 import Dice2 from "../../assets/Dice2.png";
 import Dice3 from "../../assets/Dice3.png";
+import { Link } from 'react-router-dom';
+import DuoGamesPage from '../DuoGamePage/DuoGame';
 
 const HomePage = () => {
-
     const [selectedGameType, setSelectedGameType] = useState("")
     const handleGameTypeSelection = (gameType) => {
         setSelectedGameType(gameType)
     }
+
 return (
   <section className="home-page-container">
     <div className="slogan-container">
@@ -22,10 +24,12 @@ return (
         <h3 className="title-solo">Solo</h3>
         <img className="dice-1" src={Dice1} alt="single dice" />
       </section>
+      <Link to="/duo-games" >
       <section className="duo" onClick={() => handleGameTypeSelection("duo")}>
         <h3 className="title-duo">Duo</h3>
         <img className="dice-2" src={Dice2} alt="single dice" />
       </section>
+      </Link>
       <section
         className="party"
         onClick={() => handleGameTypeSelection("party")}
