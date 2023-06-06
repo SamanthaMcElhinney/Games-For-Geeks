0604c52fbb9944645e42ffdb00bec0b111d36fd4
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 09:37:20 -0700

    add: skelton to duo game page

---
+++ src/components/DuoGamePage/DuoGame.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './DuoGame.css'
+
+const DuoGamesPage = () => {
+    return (
+        <div>
+            <h1 className="Greeting">Welcome to party for two</h1>
+        </div>
+
+    )
+}
+
+export default DuoGamesPage
