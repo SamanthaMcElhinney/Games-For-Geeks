9a9c347ddd9ad9868533eb84f4387d347d6c214c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 12:19:19 -0700

    add: destructor props passed to map over to display cards

--- src/components/DuoGamePage/DuoGame.js
+++ src/components/DuoGamePage/DuoGame.js
@@ -1,13 +1,20 @@
 import React from "react";
 import './DuoGame.css'
 
-const DuoGamesPage = () => {
-    return (
-        <div>
-            <h1 className="Greeting">Welcome to party for two</h1>
-        </div>
-
-    )
+const DuoGamesPage = ({twoPlayers}) => {
+ return (
+   <div>
+     <h1 className="Greeting">Welcome to games for two</h1>
+     <div className="game-list">
+       {twoPlayers.map((game) => (
+         <div key={game.id}>
+           <h2 className="name">Name:{game.name}</h2>
+           <p className="desc">Description:{game.description}</p>
+         </div>
+       ))}
+     </div>
+   </div>
+ );
 }
 
 export default DuoGamesPage
