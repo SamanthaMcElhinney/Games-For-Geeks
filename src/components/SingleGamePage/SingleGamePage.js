1710ec3b8bbd2b4ef16d40db5b652b248c494139
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 21:40:51 -0700

    Add single game page

---
+++ src/components/SingleGamePage/SingleGamePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./SingleGamePage.css";
+import GameCard from "../GameCard/GameCard";
+
+const SingleGamePage = ({ singlePlayer }) => {
+  if (!singlePlayer) {
+    return <p>Please hang tight while we find your games..</p>;
+  }
+
+  const cards = singlePlayer.map((game) => {
+    const ratingNum = game.average_user_rating;
+    return (
+      <GameCard
+        key={game.id}
+        title={game.name}
+        image={game.images.small}
+        rating={ratingNum.toFixed(2)}
+        playTime={game.playtime}
+      />
+    );
+  });
+
+  return <div className="card-container">{cards}</div>;
+};
+
+export default SingleGamePage;
