e7ab0fc9d20de706c8baa0cd2caafad2d654958f
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 21:42:02 -0700

    add id to game card and link matching game id to render the appropriate page

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./GameCard.css"
+import { Link } from 'react-router-dom'
 
 export default function GameCard({title, image, rating, id, playTime}) {
-    return (
-    <div className='game-card'>
-        
+  return (
+      <Link key={id} to={`/game/${id}`}>
+      <div className='game-card'>
+      <div className='title-container'>
+        <h3 className='game-title'>{title}</h3>
+      </div>
+        <img className="game-image"src={image}/>
+        <h4 className='game-rating'>Rating: {rating}/5 ⭐️</h4>
+        <h4 className="game-playtime">Play time: {playTime} mins</h4>
     </div>
+      </Link>
   )
 }
