2aa2b688d45d30e1495aee3e475e22f6e56f73aa
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 21:40:34 -0700

    Add page for group games

---
+++ src/components/GroupGamePage/GroupGamePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./GroupGamePage.css";
+import GameCard from "../GameCard/GameCard";
+
+const GroupGamePage = ({ groupPlayers }) => {
+  if (!groupPlayers) {
+    return <p>Please hang tight while we find your games..</p>;
+  }
+
+  const cards = groupPlayers.map((game) => {
+    const ratingNum = game.average_user_rating;
+    return (
+      <GameCard
+        key={game.id}
+        title={game.name}
+        image={game.images.small}
+        rating={ratingNum.toFixed(2)}
+        playTime={game.playtime}
+      />
+    );
+  });
+
+  return <div className="card-container">{cards}</div>;
+};
+
+export default GroupGamePage;
