16748eeace013ed97201fca8f788d444f1396141
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-06 21:34:47 -0700

    feat: detailed movies page on duo games, set set for duo, solo, and group

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,37 @@ import React, { useEffect, useState } from "react";
 import Header from '../Header/Header'
 import './App.css'
 import HomePage from "../HomePage/HomePage";
-import { getTwoPlayerGames } from '../../Api-Calls'
+import {
+  getTwoPlayerGames,
+  getPartyGames,
+  getSingleGames,
+} from "../../Api-Calls";
 import {Router, Switch, Route} from 'react-router-dom'
 import DuoGamesPage from "../DuoGamePage/DuoGame";
+import GroupGamePage from "../GroupGamePage/GroupGamePage";
+import SingleGamePage from "../SingleGamePage/SingleGamePage";
+import GameDetails from "../GameDetails/GameDetails";
 
 const App = () => {
-  const[twoPlayers, setTwoPlayers] = useState([])
-  const[error, setError] = useState('')
-  const[selectedGameType, setSelectedGameType] = useState([])
+  const [twoPlayers, setTwoPlayers] = useState([]);
+  const [error, setError] = useState("");
+  const [groupPlayers, setGroupPlayers] = useState([]);
+  const [singlePlayer, setSinglePlayer] = useState([]);
 
-  useEffect(()=> {
-    getTwoPlayerGames()
-    .then(data => setTwoPlayers(data.games))
-  },[])
+  useEffect(() => {
+    getTwoPlayerGames().then((data) => {
+      setTwoPlayers(data.games);
+    });
+  }, []);
 
-    console.log(twoPlayers, "twoplayers");
+  useEffect(() => {
+    getPartyGames().then((data) => setGroupPlayers(data.games));
+  }, []);
+
+  useEffect(() => {
+    getSingleGames().then((data) => setSinglePlayer(data.games));
+  }, []);
+
   return (
     <div className="App">
       <Header />
@@ -24,12 +40,34 @@ const App = () => {
         <Route exact path="/" component={HomePage} />
         <Route
           exact
+          path="/game/:id"
+          render={({ match }) => {
+            const gameId = match.params.id;
+            const game = twoPlayers.find((game) => game.id === gameId);
+            if (!game) {
+              return <p>Game not found</p>;
+            }
+            return <GameDetails game={game} />;
+          }}
+        />
+        <Route
+          exact
           path="/duo-games"
-          render={() => <DuoGamesPage twoPlayers={twoPlayers}/>}
+          render={() => <DuoGamesPage twoPlayers={twoPlayers} />}
+        />
+        <Route
+          exact
+          path="/group-games"
+          render={() => <GroupGamePage groupPlayers={groupPlayers} />}
+        />
+        <Route
+          exact
+          path="/single-game"
+          render={() => <SingleGamePage singlePlayer={singlePlayer} />}
         />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
