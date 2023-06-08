3e25b2f4ec8369b5c6d4954753937caffbb7260a
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-07 23:15:07 -0700

    refactor fetch call to make more dynamic

--- src/Api-Calls.js
+++ src/Api-Calls.js
@@ -1,7 +1,5 @@
-import React from "react";
-
-const getTwoPlayerGames = () => {
-   return fetch("https://api.boardgameatlas.com/api/search?min_players=2&client_id=t0AVnrNPcW")
+const getGames = (num) => {
+   return fetch(`https://api.boardgameatlas.com/api/search?min_players=${num}&client_id=t0AVnrNPcW`)
    .then(res => {
     if(res.ok) {
         return res.json()
@@ -11,28 +9,5 @@ const getTwoPlayerGames = () => {
    })
 }
 
-const getPartyGames = () => {
-  return fetch("https://api.boardgameatlas.com/api/search?min_players=3&client_id=t0AVnrNPcW")
-  .then(res=> {
-    if(res.ok) {
-      return res.json()
-    } else {
-      throw new Error(res.status)
-    }
-  })
-}
 
-const getSingleGames = () => {
-  return fetch("https://api.boardgameatlas.com/api/search?min_players=1&client_id=t0AVnrNPcW")
-  .then(res=> {
-    if(res.ok) {
-      return res.json()
-    } else {
-      throw new Error(res.status)
-    }
-  })
-}
-
-
-
-export { getTwoPlayerGames, getPartyGames, getSingleGames};
+export { getGames};
